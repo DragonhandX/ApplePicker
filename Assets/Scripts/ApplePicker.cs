2e70a4da118310d70f8df5c23b7063aca5f51665
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2018-09-13 19:01:23 -0400

    High Score Tracking Get
    also stopped an error from going off.  it reloads _Scene_0, and apparently turns off the Directional Light?

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -41,6 +41,12 @@ public class ApplePicker : MonoBehaviour {
         // Remove the Basket from the list and destroy the GameObject
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGo);
+
+        // If there are no Baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
     }
 	
 	// Update is called once per frame
